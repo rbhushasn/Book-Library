69b2b19e49edbf49c24371f681fe1f64af59e5e1
rbhushasn <ravi.bhushan@sial.com>
2019-05-07 21:51:29 +0530

    ajax changes

--- src/helper/ajax_helper.js
+++ src/helper/ajax_helper.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-const BASE_URL = 'http://books-by-suyashkale.herokuapp.com/APIs/';
+const BASE_URL = 'https://books-by-suyashkale.herokuapp.com/APIs/';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNyIsImlhdCI6MTU1NjUyOTg4Nn0.jVt7JjTH0yr8hSwG5x2Rp06GFD9eRsnLejdFJt6hHXk';
 
 export const ajaxHelper = ({ type, contentType, dataType, url, data, postUrl })=> {
